1518312e47dbd8a728c8bdd9c8f90df75a597540
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2021-07-23 13:13:38 +0300

    на JS сделано: зачеркивание выполненной задачи, переключение между типами задач, строки задач (кроме первой) занимают всю ширину

---
+++ TODO/JS/task_types.js
@@ -0,0 +1,49 @@
+/*switching between types of tasks */
+
+// all objects in button-centre
+let taskButton = document.getElementById("task_types");
+
+taskButton.onclick = function (event){
+    let trgt = event.target;
+
+    if(trgt.tagName!=='BUTTON') return;
+
+    showTasks(trgt.id);
+}
+
+// show only requested tasks
+function showTasks(type){
+    let alltypes=document.getElementsByClassName('todo-list__item');
+    for(let e of alltypes)
+        e.style.display='grid';
+    if(type==='all')
+    {
+        return;
+    }
+    //let list = getTaskList(type);
+
+
+    for(let elem of alltypes){
+        if(elem.classList.contains('active') && type!=='active') {
+            elem.style.display = 'none';
+        }
+        else if(!elem.classList.contains('active') && type==='active'){
+            elem.style.display = 'none';
+        }
+        }
+}
+
+
+// get list of tasks that aren't requested
+/*function getTaskList(type){
+    let alltypes=document.getElementsByClassName('todo-list__item');
+    let neededtypes =[];
+    for(let obj of alltypes){
+        obj.style.display='grid';
+        if(!obj.classList.contains(type))
+        {
+            neededtypes.push(obj);
+        }
+    };
+    return neededtypes;
+}*/

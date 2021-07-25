45209eba5521a6a54d53378396437ad9a94f121e
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2021-07-25 14:34:47 +0300

    исправлены названия функций и объявления переменных

--- TODO/JS/task_types.js
+++ TODO/JS/task_types.js
@@ -1,20 +1,20 @@
 /*switching between types of tasks */
 
 /* all objects in button-centre*/
-let taskButton = document.getElementById("task_types");
+const taskButton = document.getElementById("task_types");
 
 taskButton.addEventListener('click',showTasks)
 
 function showTasks(event){
-    let type = event.target.id;
-    DisplayTasks(type);
-    ChangeCurrentTaskType(type);
+    const type = event.target.id;
+    displayTasks(type);
+    changeCurrentTaskType(type);
 }
 
 /* displays tasks of required types*/
-function DisplayTasks(type){
-    let allTypes=document.getElementsByClassName('todo-list__item');
-    for(let elem of allTypes){
+function displayTasks(type){
+    const allTypes=document.getElementsByClassName('todo-list__item');
+    for(const elem of allTypes){
         if(elem.classList.contains(type) || type==='all') {
             elem.style.display = 'grid';
         }
@@ -24,9 +24,9 @@ function DisplayTasks(type){
     }
 }
 
-/* assign class current-task-type to current type of task */
-function ChangeCurrentTaskType(type){
-    let curTask = document.querySelector('.current-task-type');
+/* assigns class current-task-type to current type of task */
+function changeCurrentTaskType(type){
+    const curTask = document.querySelector('.current-task-type');
     if(curTask.id === type) return;
     curTask.classList.remove('current-task-type');
     document.getElementById(type).classList.add('current-task-type');
